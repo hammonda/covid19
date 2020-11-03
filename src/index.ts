045355ab7daf9feb0b8417219c92b944e866c2e5
hammonda <david.hammond@bentham.co.uk>
2020-11-03 20:47:28 +0000

    add markers

--- src/index.ts
+++ src/index.ts
@@ -42,22 +42,54 @@ loadData().then(data => {
   const cases = getRolling(data.cases, 7.0, 7.0);
   const active = getRolling(cases, 14.0, 1.0);
   const r = _.map(active, (v, i) => 14.0 * cases[i] / v);
+  const deaths = getRolling(data.deaths, 7, 7);
   const colors = _.map(r, v => (v > 1) ? 'red' : 'green');
+
+  const ht_average = 'Deaths (rolling average): %{z}<br>R: %{x}<br>Active cases: %{y}';
+  const ht_latest = 'Deaths: %{z}<br>R: %{x}<br>Active cases: %{y}';
 
   Plotly.newPlot('root', [
     {
       x: r,
-      y: active, // rolling sum of last 14 days
-      z: getRolling(data.deaths, 7, 7), // rolling average of last 7 days
+      y: active,
+      z: deaths,
       type: 'scatter3d',
-      mode: 'lines',
+      mode: 'lines+markers',
       line: {
         color: colors,
-        width: 1,
+        width: 3,
         shape: 'spline'
       },
-      hovertemplate: 'New deaths: %{z}<br>R: %{x}<br>Active cases: %{y}'
+      marker: {
+        size: 5,
+        opacity: 0.25
+      },
+      hovertemplate: ht_average
     },
+    {
+      x: [r[0]],
+      y: [active[0]],
+      z: [deaths[0]],
+      type: 'scatter3d',
+      mode: 'markers',
+      marker: {
+        color: r[0] > 1.0 ? 'red': 'green',
+        opacity: 0.5
+      },
+      hovertemplate: ht_average
+    },
+    {
+      x: [r[0]],
+      y: [active[0]],
+      z: [data.deaths[0]],
+      type: 'scatter3d',
+      mode: 'markers',
+      marker: {
+        color: 'blue',
+        opacity: 0.5
+      },
+      hovertemplate: ht_latest
+    }
     /*
     {
       type: 'mesh3d',
@@ -77,18 +109,16 @@ loadData().then(data => {
     scene: {
       xaxis: {
         title: 'R',
-        type: 'linear',
-        range: [ 0, 3 ],
+        range: [0, 3],
       },
       yaxis: {
         title: 'Active cases',
         type: 'log',
-        range: [ 3, 5.69897 ]
+        range: [3, 5.69897]
       },
       zaxis: {
-        title: 'New deaths each day',
-        //type: 'log',
-        range: [ 0, 1000 ],
+        title: 'Deaths',
+        range: [0, 1000],
       },
       camera: {
         eye: {x: -1.9257754289657114, y: -0.8855855700861778, z: 0.18474927520586074},
@@ -103,9 +133,31 @@ loadData().then(data => {
       t: 0
     }
   });
+
 /*
   document.getElementById('root').on('plotly_relayout', event => {
     console.log(event);
   });
+*/
+
+/*
+  let i = 0;
+  function update() {
+    console.log(i);
+    i = (i + 1) % active.length;
+    Plotly.animate('root', {
+      data: [,{x: [r[i]], y: [active[i]], z: [deaths[i]]}]
+    }, {
+      transition: {
+        duration: 0,
+      },
+      frame: {
+        duration: 0,
+        redraw: true,
+      }
+    });
+    window.requestAnimationFrame(update);
+  }
+  window.requestAnimationFrame(update);
 */
 });
