17fc06c672357f2c2df55d955cbc3b59ffb8cf4f
hammonda <david.hammond@bentham.co.uk>
2021-01-31 22:16:31 +0000

    switch to fs.promises

--- load.ts
+++ load.ts
@@ -5,19 +5,27 @@
 //
 
 import axios from 'axios';
-import fs from 'fs';
+import { promises as fs } from 'fs';
 import { JSDOM } from 'jsdom';
 
-axios.get('https://www.worldometers.info/world-population/population-by-country/')
-  .then(html => {
-  const { window } = new JSDOM(html.data);
+// Scrape country population data from https://www.worldometers.info/
+const loadPopulationData = async (): Promise<{[key: string]: number}> => {
+  const html = (await axios.get('https://www.worldometers.info/world-population/population-by-country/'
+    , { timeout: 2000})).data;
+  const { window } = new JSDOM(html);
   const $ = require('jquery')(window) as JQueryStatic;
-  const data: any = {};
+  const data: {[key: string]: number} = {};
   $('tr').each(function() {
     const td = $(this).find('td');
     if ($(td[0]).html()) {
       data[$(td[1]).text()] = Number($(td[2]).text().replace(/,/g,''));
     }
   });
-  fs.writeFileSync('src/data/population.json', JSON.stringify(data));
-});
+  return data;
+};
+
+// Load and save data
+loadPopulationData()
+.then(data => fs.writeFile('src/data/population.json', JSON.stringify(data)))
+.then(() => console.log('done!'))
+.catch(err => console.error(err));
